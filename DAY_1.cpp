3ac099dc9ab9258053298dcf2c4c275b75c4fc18
SunilChauhan2 <sunilchauhan27c@gmail.com>
2025-02-15 21:20:00 +0530

    added first day

---
+++ DAY_1.cpp
@@ -0,0 +1,18 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int n,m,l;
+//     cin>> n >> m >>l;
+//     cout<<" "<< n <<" "<< m <<" "<< l; 
+    
+//    return 0;
+// }
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main (){
+    float x,y;
+    cin>>x>>y;
+    cout<<fixed<< setprecision(2) << x+y;
+}
