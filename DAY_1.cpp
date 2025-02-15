ac100dc152bd18c286107bf99cc988a92cd97381
SunilChauhan2 <sunilchauhan27c@gmail.com>
2025-02-15 23:13:33 +0530

    Commented the Questions

--- DAY_1.cpp
+++ DAY_1.cpp
@@ -1,12 +1,12 @@
-// Question 1:Write a program to show how to read and write integer values from user.
-//  Input Format:
-// Each test case contains 3 integers separated by space.
-// Output Format:
-// Print the 3 integers separated by space.
-// Sample Input
-// 10 20 30
-// Sample Output
-// 10 20 30
+/*Question 1:Write a program to show how to read and write integer values from user.
+ Input Format:
+Each test case contains 3 integers separated by space.
+Output Format:
+Print the 3 integers separated by space.
+Sample Input
+10 20 30
+Sample Output
+10 20 30*/
 #include<iostream>
 using namespace std;
 int main()
@@ -17,11 +17,11 @@ int main()
     return 0;
 }
 
-//Question 2:Input two numbers x and y from keyboard you need to print the sum of two numbers with 2 decimal digit.
-// Sample input
-// 10.2 13.89
-// sample output
-// 14.09
+/*Question 2:Input two numbers x and y from keyboard you need to print the sum of two numbers with 2 decimal digit.
+Sample input
+10.2 13.89
+sample output
+14.09*/
 #include<iostream>
 #include<iomanip>
 using namespace std;
